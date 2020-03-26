6f3ed0f80611f21a7ed111b0e2f07134f97b9dab
James Johnson <jtaylorjohnson@gmail.com>
2020-03-26 18:02:20 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,8 +1,7 @@
 def reverse_each_word(word_string)
   array = word_string.split
   array.collect do |word|
-    sentence = word.reverse
-    
+    sentence << word.reverse
   end
- 
+  puts "#{sentence}"
 end
