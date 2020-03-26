d1ea841fd4116ca08a4ac6ef035dae82ba94cdc2
James Johnson <jtaylorjohnson@gmail.com>
2020-03-26 17:43:26 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,6 +1,7 @@
 def reverse_each_word(word_string)
   array = word_string.split
   array.collect do |word|
-    sentence = word.reverse 
+    sentence = word.reverse
+    return sentence
   end
 end
